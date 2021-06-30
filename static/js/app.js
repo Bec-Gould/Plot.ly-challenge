5dc89f62deee2a0ee3f353024d06fa23f5a53c13
Bec-Gould <rgould1986@hotmail.com>
2021-06-30 18:41:23 +0800

    added code for plots

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,9 @@
-// copied from class exercise
+// read json file
 d3.json("samples.json").then((data) => {
   // console.log(importedData);
   console.log(data)
 });
+
 
 
 
@@ -21,6 +22,17 @@ function getMetadata(selection) {
 
 getMetadata("940")
 
+
+function init() {
+  d3.json("samples.json").then(data => {
+    console.log(data);
+    
+}
+
+
+
+
+
 function buildPlots(selection) {
   d3.json("samples.json").then(data => {
     var samples = data.samples;
@@ -28,7 +40,47 @@ function buildPlots(selection) {
     // console.log(filteredData)
     var OTU_id = filteredData.otu_ids;
     var sample_values = filteredData.sample_values;
-    var OTU_labels = filteredData.out_labels;
+    var OTU_labels = filteredData.otu_labels;
+    var barlabels = OTU_id.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+    console.log(OTU_id)
+    console.log(sample_values)
+    console.log(OTU_labels)
+
+
+    var trace1 = {
+      type: "bar",
+      text: OTU_labels.slice(0,10).reverse(),
+      x: sample_values.slice(0,10).reverse(),
+      y: barlabels.slice(0,10),
+      orientation: "h",
+    }
+
+    var bardata = [trace1];
+
+    Plotly.newPlot("bar", bardata);
+
+    var trace2 = {
+      x: OTU_id,
+      y: sample_values,
+      mode: "markers",
+      marker: {
+        color: OTU_id,
+        size: sample_values,
+      } 
+    }
+
+    var bubbledata = [trace2];
+
+    var layout = {
+      title: "OTU ID",
+      showlegend: false,
+      height: 600,
+      width: 1200,
+
+    }
+
+    Plotly.newPlot("bubble", bubbledata, layout);
+
 });
 };
 
@@ -36,11 +88,42 @@ buildPlots("940")
 
 
 
-  // Slice the first 10 objects for plotting
-  // data = data.slice(0, 10);
 
-  // // Reverse the array due to Plotly's defaults
-  // data = data.reverse();
+
+
+d3.selectAll("body").on("change", menuChange);
+
+function menuChange(selection) {
+  // Use D3 to select the dropdown menu
+  var dropdownMenu = d3.selectAll("#selDataset").node();
+  // Assign the dropdown menu item ID to a variable
+  var dropdownMenuID = dropdownMenu.id;
+  // Assign the dropdown menu option to a variable
+  var selectedOption = dropdownMenu.value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   Slice the first 10 objects for plotting
+//   data = data.slice(0, 10);
+
+//   // Reverse the array due to Plotly's defaults
+//   data = data.reverse();
 
 //   // Trace1 for the Greek Data
 //   var trace1 = {
